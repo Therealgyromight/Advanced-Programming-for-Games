69956fb3aa8e0a5635dd6acc297bfd430c08638b
Rui Antunes <b9059301@newcastle.ac.uk>
2019-09-30 15:09:29 +0100

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int a = 5; 
+	int b = 5;
+	int c = 5;
+	std::cout << "Hello World!" << std::endl;
+	
+    return 0;
+	
+}
